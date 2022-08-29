64636f2705fff208bda9797592ba8de8a890742e
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-08-29 22:07:08 +0900

    resize font size

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <userContext.Provider value={auth}>
       <div className="App">
         <Messages scroll={scroll} />
-        <div ref={scroll} className="scroll-target"></div>
+        <div ref={scroll}></div>
         {user ? <FormField scroll={scroll} /> : <SignIn />}
       </div>
     </userContext.Provider>
