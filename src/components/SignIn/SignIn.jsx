c8177446076603e089952c4ee17aae26d2f2a192
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-09-04 06:36:12 +0900

    test popup auth

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,6 +3,7 @@ import {
   signInWithRedirect,
   GoogleAuthProvider,
   TwitterAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { userContext } from "../../App";
 import Button from "../utility/Button";
@@ -36,14 +37,67 @@ const SignIn = () => {
           </p>
           <Button
             buttonClass="auth-btn google-btn"
-            buttonAction={() => signInWithRedirect(auth, googleProvider)}
+            buttonAction={() => {
+              signInWithPopup(auth, googleProvider)
+                .then((result) => {
+                  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+                  // You can use these server side with your app's credentials to access the Twitter API.
+                  const credential =
+                    GoogleAuthProvider.credentialFromResult(result);
+                  const token = credential.accessToken;
+                  const secret = credential.secret;
+
+                  // The signed-in user info.
+                  const user = result.user;
+                  // ...
+                })
+                .catch((error) => {
+                  // Handle Errors here.
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // The email of the user's account used.
+                  const email = error.customData.email;
+                  // The AuthCredential type that was used.
+                  const credential =
+                    GoogleAuthProvider.credentialFromError(error);
+                  console.log(errorCode);
+                  console.log(errorMessage);
+                  console.log(credential);
+                  // ...
+                });
+            }}
             logo="google.svg"
           >
             Googleでサインイン
           </Button>
           <Button
             buttonClass="auth-btn twitter-btn"
-            buttonAction={() => signInWithRedirect(auth, twitterProvider)}
+            buttonAction={() => {
+              signInWithPopup(auth, twitterProvider)
+                .then((result) => {
+                  // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+                  // You can use these server side with your app's credentials to access the Twitter API.
+                  const credential =
+                    TwitterAuthProvider.credentialFromResult(result);
+                  const token = credential.accessToken;
+                  const secret = credential.secret;
+
+                  // The signed-in user info.
+                  const user = result.user;
+                  // ...
+                })
+                .catch((error) => {
+                  // Handle Errors here.
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // The email of the user's account used.
+                  const email = error.customData.email;
+                  // The AuthCredential type that was used.
+                  const credential =
+                    TwitterAuthProvider.credentialFromError(error);
+                  // ...
+                });
+            }}
             logo="twitter.svg"
           >
             Twitterでサインイン
